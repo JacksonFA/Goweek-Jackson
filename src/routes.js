644e2ecce024fd6cd2152dfb05b2e18f342d746e
jacksonFA <jackson.f205@gmail.com>
2018-11-06 23:48:22 -0200

    Primeiro ponto no projeto

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const routers = express.Router();
+
+const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
+
+routers.get('/tweets', TweetController.index);
+routers.post('/tweets', TweetController.store);
+routers.post('/likes/:id', LikeController.store);
+
+module.exports = routers;
